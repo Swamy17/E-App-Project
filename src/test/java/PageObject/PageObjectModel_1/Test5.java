832850aec1bd9e8bce45a093478593afbf55cf87
swamy17 <tkumaraswamy19@gmail.com>
2020-09-01 19:34:53 +0530

    Added New Method

---
+++ src/test/java/PageObject/PageObjectModel_1/Test5.java
@@ -0,0 +1,20 @@
+package PageObject.PageObjectModel_1;
+
+public class Test5 {
+
+	public static void m1(int a)
+	{
+		int b;
+		b=a;
+		System.out.println(b);
+	}
+	
+	public static void main(String[] args) {
+	
+		
+		Test5 a=new Test5();
+		a.m1(8);
+
+	}
+
+}
